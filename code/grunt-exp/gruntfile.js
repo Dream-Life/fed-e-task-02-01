a20c2db1a1aae6879c96ecd188a63c635c230296
Dream-Life <l>
2020-06-13 16:17:34 +0800

    提交

--- code/grunt-exp/gruntfile.js
+++ code/grunt-exp/gruntfile.js
@@ -1,1 +1,175 @@
 // 实现这个项目的构建任务
+const grunt = require('grunt')
+const loadGruntTasks = require('load-grunt-tasks')
+
+const data = {
+    menus: [
+      {
+        name: 'Home',
+        icon: 'aperture',
+        link: 'index.html'
+      },
+      {
+        name: 'Features',
+        link: 'features.html'
+      },
+      {
+        name: 'About',
+        link: 'about.html'
+      },
+      {
+        name: 'Contact',
+        link: '#',
+        children: [
+          {
+            name: 'Twitter',
+            link: 'https://twitter.com/w_zce'
+          },
+          {
+            name: 'About',
+            link: 'https://weibo.com/zceme'
+          },
+          {
+            name: 'divider'
+          },
+          {
+            name: 'About',
+            link: 'https://github.com/zce'
+          }
+        ]
+      }
+    ],
+    pkg: require('./package.json'),
+    date: new Date()
+  }
+
+module.exports = grunt=>{
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        clean:['dist','.temp'],
+        copy: {
+            main: {
+              files: [
+                // includes files within path
+                {expand: true, src: ['public/**'], dest: 'dist/'},
+                // {expand: true, src: ['src/assets/images/**'], dest: 'dist/assets/images/', filter: 'isFile'},
+                // {expand: true, src: ['src/assets/fonts/**'], dest: 'dist/assets/fonts/', filter: 'isFile'},
+              ],
+            }
+          },
+        sass:{
+            main: {
+                files: {
+                    'dist/assets/styles/main.css' : 'src/assets/styles/main.scss',
+                    '.temp/main.css' : 'src/assets/styles/main.scss',
+                  }
+            }
+        },
+        babel: {
+            options: {
+              sourceMap: true,
+              presets: ['@babel/preset-env']
+            },
+            main: {
+              files: {
+                'dist/assets/scripts/main.js' : 'src/assets/scripts/main.js',
+                '.temp/main.js' : 'src/assets/scripts/main.js',
+              }
+            }
+          },
+          watch: {
+            js: {
+              files: ['src/assets/scripts/*.js'],
+              tasks: ['babel']
+            },
+            css: {
+              files: ['src/assets/styles/*.scss'],
+              tasks: ['sass']
+            },
+            html:{
+              files: ['src/*.html'],
+              tasks: ['swigtemplates','copy:swig']
+            }
+          },
+        uglify:{
+            dist:{
+                src:'.temp/main.js',
+                dest:'dist/assets/scripts/main.js'
+            }
+        },
+        cssmin: {
+            dist: {
+              files: {
+                'dist/assets/styles/main.css': '.temp/main.css'
+              }
+            }
+          },
+          swigtemplates: {
+            temp: {
+              context: data,
+              dest: '.temp',
+              src: 'src/*.html'
+            }
+          },
+          browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'dist/assets/styles/*.css',
+                        'dist/assets/scripts/*.js',
+                        'dist/*.html',
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: ['dist', 'src', 'public'],
+                        routes: {
+                          '/node_modules': 'node_modules'
+                        }
+                      }
+                }
+            }
+        },
+        htmlmin: {
+            options: {
+                removeComments: true,
+                collapseWhitespace: true,
+                minifyCSS: true,
+                minifyJS: true
+            },
+            dist: {
+                expand: true,
+                cwd: '.temp/src',
+                src: '*.html',
+                dest: 'dist'
+            }
+        },
+        move: {
+            index: {
+              src: '.temp/src/index.html',
+              dest: 'dist/index.html'
+            },
+            about: {
+              src: '.temp/src/about.html',
+              dest: 'dist/about.html'
+            }
+          },
+          imagemin: {
+            imagesandfonts: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: ['assets/images/*..{jpg,jpeg,gif,png,svg}','assets/fonts/*.{svg,eot,ttf,woff}'],
+                    dest: 'dist/'
+                }]
+            }
+        }
+    })
+
+    loadGruntTasks(grunt)
+
+    grunt.registerTask('clear',['clean'])
+    grunt.registerTask('start',['clean','sass','babel','swigtemplates','move','browserSync','watch'])
+    grunt.registerTask('build',['clean','sass','babel','copy','swigtemplates','cssmin','uglify','htmlmin','imagemin'])
+}
--- code/gulp-exp/gulpfile.js
+++ code/gulp-exp/gulpfile.js
@@ -1,13 +1,159 @@
 // 实现这个项目的构建任务
-
-const loadPlugin = require()
+const {src, dest, parallel, series, watch} = require('gulp')
+const loadPlugin = require('gulp-load-plugins')
+const del = require('del')
+const browserSync =require('browser-sync')
 
 const plugin = loadPlugin()
+const bs = browserSync.create()
 
+const data = {
+    menus: [
+      {
+        name: 'Home',
+        icon: 'aperture',
+        link: 'index.html'
+      },
+      {
+        name: 'Features',
+        link: 'features.html'
+      },
+      {
+        name: 'About',
+        link: 'about.html'
+      },
+      {
+        name: 'Contact',
+        link: '#',
+        children: [
+          {
+            name: 'Twitter',
+            link: 'https://twitter.com/w_zce'
+          },
+          {
+            name: 'About',
+            link: 'https://weibo.com/zceme'
+          },
+          {
+            name: 'divider'
+          },
+          {
+            name: 'About',
+            link: 'https://github.com/zce'
+          }
+        ]
+      }
+    ],
+    pkg: require('./package.json'),
+    date: new Date()
+  }
+
+const paths = {
+    styles: 'src/assets/styles/*.scss',
+    script: 'src/assets/scripts/*.js',
+    html: 'src/*.html',
+    image:'src/assets/images/**',
+    font: 'src/assets/fonts/**',
+    extra:'public/**'
+}
+
+const distPath = 'dist'
+const tempPath = '.temp'
+
+// 清除dist目录
 const clean = ()=>{
+    return del([distPath])
+}
+
+// css文件处理
+const styles = ()=>{
+    return src(paths.styles,{base: 'src'})
+            .pipe(plugin.sass())
+            .pipe(dest(distPath))
+            .pipe(bs.reload({ stream: true }))
+}
+
+// js文件处理
+const script = ()=>{
+    return  src(paths.script,{base: 'src'})
+            .pipe(plugin.babel({presets: ["@babel/preset-env"]}))
+            .pipe(dest(distPath))
+            .pipe(bs.reload({ stream: true }))
 
 }
 
-const styles = ()=>{
+// html处理
+const html = ()=>{
+    return src(paths.html,{base: 'src'})
+            .pipe(plugin.swig({ data, defaults: { cache: false } })) // 防止模板缓存导致页面不能及时更新
+            .pipe(dest(tempPath))
+            .pipe(dest(distPath))
+            .pipe(bs.reload({ stream: true }))
+}
 
+// 图片和字体文件处理
+const image = ()=>{
+    return src(paths.image,{base:'src'})
+            .pipe(plugin.imagemin())
+            .pipe(dest(distPath))
+}
+
+const font = ()=>{
+    return src(paths.font,{base:'src'})
+            .pipe(plugin.imagemin())
+            .pipe(dest(distPath))
+}
+
+// public文件copy
+const extra = () => {
+    return src(paths.extra, { base: 'public' })
+      .pipe(dest('dist'))
+}
+
+// serve
+const serve = ()=>{
+    watch(paths.styles,styles)
+    watch(paths.script,script)
+    watch(paths.html,html)
+    watch([
+        paths.extra,
+        paths.image,
+        paths.font
+    ],bs.reload)
+
+    bs.init({
+        notify:false,
+        port:8080,
+        server: {
+            baseDir: [distPath, 'src', 'public'],
+            routes: {
+              '/node_modules': 'node_modules'
+            }
+          }
+    })
+}
+
+// useref
+const useref = ()=>{
+    return src(`${tempPath}/*.html`,{base: tempPath})
+            .pipe(plugin.useref({ searchPath: [tempPath,'.']}))
+            .pipe(plugin.if(/\.js$/, plugin.uglify()))
+            .pipe(plugin.if(/\.css$/, plugin.cleanCss()))
+            .pipe(plugin.if(/\.html$/, plugin.htmlmin({
+                collapseWhitespace: true,
+                minifyCSS: true,
+                minifyJS: true
+              })))
+            .pipe(dest(distPath))
+}
+
+const compile = parallel(styles,script,html)
+
+const start = series(clean,compile,serve)
+const build = series(clean,parallel(series(compile, useref),font,extra,image))
+
+module.exports = {
+    clean,
+    start,
+    build
 }
